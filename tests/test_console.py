e723f411bfc2bebf61be3fefe7a71acce9cff833
Cheikyadam <urbaingande@gmail.com>
2024-02-19 17:32:59 -0800

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,7 @@ from models import storage
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
+import os
 
 class ConsoleTestCase(unittest.TestCase):
     """Console test case"""
@@ -22,16 +23,16 @@ class ConsoleTestCase(unittest.TestCase):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
+        except FileNotFoundError:
             pass
 
     def test_good_format(self):
         """ """
         with patch('sys.stdout', new=StringIO()) as f:
-                HBNBCommand().onecmd("create State name=\"California\"")
+            HBNBCommand().onecmd("create State name=\"California\"")
         obj = storage.all()
         for key, value in obj.items():
-            valuedic= value.to_dict()
+            valuedic = value.to_dict()
             isPresent_att = 'name' in valuedic
             self.assertTrue(isPresent_att)
             self.assertTrue(type(valuedic['name'] is str))
@@ -40,22 +41,24 @@ class ConsoleTestCase(unittest.TestCase):
     def test_good_format_many_param(self):
         """ """
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("""create Place city_id="0001" user_id="0001" name="My_little_house"
-                    number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300
+            HBNBCommand().onecmd("""create Place city_id="0001"
+                    user_id="0001" name="My_little_house"
+                    number_rooms=4 number_bathrooms=2
+                    max_guest=10 price_by_night=300
                     latitude=37.773972 longitude=-122.431297""")
-        obj = storage.all()
+            obj = storage.all()
         for key, value in obj.items():
-            valuedic= value.to_dict()
+            valuedic = value.to_dict()
             isPresent_att = 'city_id' in valuedic
             self.assertTrue(isPresent_att)
             self.assertTrue(type(valuedic['city_id'] is str))
             self.assertTrue(valuedic['city_id'] == "0001")
-       
+
             isPresent_att = 'user_id' in valuedic
             self.assertTrue(isPresent_att)
             self.assertTrue(valuedic['user_id'] == "0001")
             self.assertTrue(type(valuedic['user_id'] is str))
-            
+
             isPresent_att = 'name' in valuedic
             self.assertTrue(isPresent_att)
             self.assertTrue(valuedic['name'] == "My little house")
@@ -71,29 +74,30 @@ class ConsoleTestCase(unittest.TestCase):
             self.assertTrue(valuedic['latitude'] == 37.773972)
 
             self.assertTrue(valuedic['longitude'] == -122.431297)
-    
+
     def test_bad_format_param(self):
         """Comment here"""
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create Place city_id")
         obj = storage.all()
         for key, value in obj.items():
-            valuedic= value.to_dict()
+            valuedic = value.to_dict()
             isPresent_att = 'city_id' in valuedic
             self.assertFalse(isPresent_att)
 
     def test_bad_format_many(self):
         """Comment here"""
         with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create Place city_id=\"0001\" user_id = \"001\"")
+            HBNBCommand().onecmd("create Place city_id=\"0001\"\
+                    user_id = \"001\"")
         obj = storage.all()
         for key, value in obj.items():
-            valuedic= value.to_dict()
+            valuedic = value.to_dict()
             self.assertTrue('city_id' in valuedic)
             self.assertFalse('user_id' in valuedic)
             self.assertFalse('=' in valuedic)
             self.assertFalse('001' in valuedic)
 
-            
+
 if __name__ == "__main__":
     unittest.main()
