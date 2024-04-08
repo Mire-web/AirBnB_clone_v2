ad477b140afb4be27c369b544b750c8a12bb39ec
mire-web <amahe8664@gmail.com>
2024-04-08 22:53:57 +0100

    set2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,7 @@ sudo apt-get update
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 echo "It Works and Mirey made it" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 old_string="server_name _;"
