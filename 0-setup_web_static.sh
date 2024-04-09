0791f66bbba2f2c68f466c087a68b3b903fc4b19
mire-web <amahe8664@gmail.com>
2024-04-09 17:07:53 +0100

    set4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,11 +5,11 @@ sudo apt-get update
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+echo "<h1>Fucking Mirey 👽<h1/>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-echo "It Works and Mirey made it" > /data/web_static/releases/test/index.html
 old_string="server_name _;"
-new_string="server_name _;\\nlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t\\tautoindex off;\\n\\t}"
+new_string="server_name _;#\\nlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t\\tautoindex off;\\n\\t}"
 sudo sed -i "s|$old_string|$new_string|" /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx restart
