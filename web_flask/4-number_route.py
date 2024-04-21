cc072afde3ef8e3cf71fbbd4936ee5190ec86266
mire-web <amahe8664@gmail.com>
2024-04-21 14:03:01 +0100

    number_routee

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Run Flask App on 5000 from 0.0.0.0
+Author: Mire-web
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontext(text):
+    if text:
+        return f"Python {text.replace('_', ' ')}"
+    else:
+        return f"Python {text}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def displayNumber(n):
+    if int(n):
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)
