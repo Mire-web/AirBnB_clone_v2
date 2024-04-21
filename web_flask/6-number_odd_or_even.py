f07c5cda0635d0b837fe9a8482cb48ce98ae5dee
mire-web <amahe8664@gmail.com>
2024-04-21 09:07:22 -0700

    fixed od or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,6 +53,5 @@ def odoreven(n):
         return render_template('6-number_odd_or_even.html', n=n, isEven=False)
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
